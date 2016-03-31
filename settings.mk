57f1e59a39829e37381be1c5300432f2667e0c98
Jeffrey Clark <dude@zaplabs.com>
2016-03-30 23:24:05 -0500

    add my settings

---
+++ settings.mk
@@ -0,0 +1,8 @@
+JOBS := 8
+MXE_TARGETS := i686-w64-mingw32.static x86_64-w64-mingw32.static
+MXE_GCC_THREADS := posix
+MXE_PLUGIN_DIRS := plugins/gcc5
+#SOURCEFORGE_MIRROR := downloads.sourceforge.net
+#LOCAL_PKG_LIST := boost curl file flac lzo pthreads vorbis wxwidgets
+#.DEFAULT local-pkg-list:
+#local-pkg-list: $(LOCAL_PKG_LIST)
